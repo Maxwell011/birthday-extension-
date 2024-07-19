014586de3524e76f5977451885373558ff4a9cfb
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-07-19 22:32:19 +0100

    Comment out unused code in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>This is the home page for the extension</h1>
-    </main>
-  );
-}
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1>This is the home page for the extension</h1>
+//     </main>
+//   );
+// }
